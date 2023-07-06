4213b8c0614a3615f389d2f344fc011716ceddd7
ryu kiyuna <s23006@std.it-college.ac.jp>
2023-07-06 05:29:41 +0000

    CHAPTER02/

---
+++ CHAPTER02/Q2_7_2.py
@@ -0,0 +1,9 @@
+# 昇順
+my_list = ["orange", "apple", "grape", "banana"]
+my_list.sort()
+print(my_list)
+
+# 降順
+my_list = ["orange", "apple", "grape", "banana"]
+my_list.sort(reverse=True)
+print(my_list)
