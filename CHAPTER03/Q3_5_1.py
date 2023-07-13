ad0a6c47ea5283a16cf64154f691aeba90206e8f
ryu kiyuna <s23006@std.it-college.ac.jp>
2023-07-13 07:10:27 +0000

    Q3_5_1.py

---
+++ CHAPTER03/Q3_5_1.py
@@ -0,0 +1,16 @@
+a = 1
+while True:
+    if a % 2 == 0:
+        print(a)
+    a += 1
+    if a >= 10:
+        break
+
+# 奇数を表示する場合
+A = 1
+while True:
+    if A % 2 != 0:
+        print(A)
+    A += 1
+    if A >= 10:
+        break
