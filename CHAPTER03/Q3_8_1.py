ae0f6694adce41ce3501b3f3a84556573cf67392
ryu kiyuna <s23006@std.it-college.ac.jp>
2023-07-18 07:33:07 +0000

    Q3_8_1.py

--- CHAPTER03/Q3_8_1.py
+++ CHAPTER03/Q3_8_1.py
@@ -1,6 +1,7 @@
+# 1から20まで表示する場合
 import pickle
 
 with open("test1.pkl", "wb") as f:
-    my_list = list(range(1, 11))
+    my_list = list(range(1, 21))
     pickle.dump(my_list, f)
     print(my_list)
