8a20aa90ab8c471f88b6484b686f4b3bf38dbd94
ryu kiyuna <s23006@std.it-college.ac.jp>
2023-07-19 06:14:12 +0000

    Q4_1_1.py

--- CHAPTER04/Q4_1_1.py
+++ CHAPTER04/Q4_1_1.py
@@ -1,7 +1,11 @@
 def fib(n):
+    """nより小さなフィボナッチ数列を列挙する"""
     a, b = 0, 1
     while a < n:
-        c = a + b
-        print(c)
-        a = b
-        b = c
+        print(a, end=" ")
+        a, b = b, a + b
+
+
+# テスト
+n = 1000  # フィボナッチ数列の上限
+fib(n)
