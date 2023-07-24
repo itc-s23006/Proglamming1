3daec46a3bc0f0bae21910723bce8995991bebd0
ryu kiyuna <s23006@std.it-college.ac.jp>
2023-07-24 06:17:13 +0000

    Q4_2_2.py

---
+++ CHAPTER04/Q4_2_2.py
@@ -0,0 +1,18 @@
+def perrin(m=100):
+    """
+    mより小さなペラン数列をリストで返す
+    ペラン数列: 3, 0, 2, 3, 2, 5, 5, 7, 10, 12, 17, 22, 29, 39, ...
+    """
+    a, b, c = 3, 0, 2
+    result = []
+    while a < m:
+        result.append(a)
+        a, b, c = b, c, a + b
+    return result
+
+
+# perrin関数を使って結果を出力するプログラム
+if __name__ == "__main__":
+    m = 100  # mの値を指定（任意の数値）
+    perrin_sequence = perrin(m)
+    print(perrin_sequence)
