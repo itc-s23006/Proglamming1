f80e18bee9192741fd35d623d36342eb3e1c2b19
ryu kiyuna <s23006@std.it-college.ac.jp>
2023-07-25 01:35:32 +0000

    Q4_3_1.py

---
+++ CHAPTER04/Q4_3_1.py
@@ -0,0 +1,9 @@
+def concat_words(*args, separator="."):
+    """任意の数の位置引数と区切り文字を受け取り、区切り文字でつなげる"""
+    return separator.join(args)
+
+
+result1 = concat_words("a", "b", "c", "d", separator="_")
+result2 = concat_words("OSAKA", "okinawa", "AKITA" separator="*")
+print(result1)
+print(result2)
