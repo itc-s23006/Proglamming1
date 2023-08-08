af9b4dd6e75b7d7cee8f2767a016ab04afeefdd2
ryu kiyuna <s23006@std.it-college.ac.jp>
2023-08-08 07:42:57 +0000

    Q5_3_2.py

---
+++ CHAPTER05/Q5_3_1.py
@@ -0,0 +1,9 @@
+members = ["Yamada", "Satou", "Suzuki", "Tanaka", "Itou"]
+del members[2]
+members
+print(members)
+# Tamashiroを削除
+members = ["Kaki", "Tamashiro", "Kiyuna"]
+del members[1]
+members
+print(members)
