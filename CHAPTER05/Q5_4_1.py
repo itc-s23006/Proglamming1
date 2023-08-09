02597cf950165d5a0a49d6641d5b9fce648d76e5
ryu kiyuna <s23006@std.it-college.ac.jp>
2023-08-09 07:20:00 +0000

    Q5_4_1.py

---
+++ CHAPTER05/Q5_4_1.py
@@ -0,0 +1,10 @@
+a, b, c, d, e = ("Sapporo", "Sendai", "Tokyo", "Nagoya", "Osaka")
+result = g = d + e
+g
+print(result)
+
+# OsakaOkinawa と出力する場合
+K, N, H, R = ("Biei", "Sendai", "Osaka", "Okinawa")
+result = A = H + R
+A
+print(result)
