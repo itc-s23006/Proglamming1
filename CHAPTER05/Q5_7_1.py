51047f982755cc33292085e0d39c4818e7495eab
ryu kiyuna <s23006@std.it-college.ac.jp>
2023-08-15 06:37:09 +0000

    Q5_7_1.py

---
+++ CHAPTER05/Q5_7_1.py
@@ -0,0 +1,8 @@
+yamanote_line = {0: "Tokyo", 1: "Yuurakuchou", 2: "Shinbashi"}
+for v in yamanote_line.values():
+    print(v)
+
+# 'Hokkaido', 'Okinawa', 'Osaka', 'Tokyo'
+japan_line = {0: "Hokkaido", 1: "Okinawa", 2: "Osaka", 3: "Tokyo"}
+for j in japan_line.values():
+    print(j)
