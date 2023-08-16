38d43269e1b30828840187a58a17fa846f68f09b
ryu kiyuna <s23006@std.it-college.ac.jp>
2023-08-16 02:01:41 +0000

    Q5_10_1.py

---
+++ CHAPTER05/Q5_10_1.py
@@ -0,0 +1,16 @@
+def is_number_of_positions_of_10(x):
+    return 10 <= x < 20
+
+
+result = is_number_of_positions_of_10(12)
+print(result)
+
+# K = 22
+
+
+def number(K):
+    return 22 < K < 33
+
+
+result = number(22)
+print(result)
